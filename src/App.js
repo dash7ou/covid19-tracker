7871a8c1d9b7d523c3027b03d5d6adcaf7d4311c
DasH <fxfddfno@gmail.com>
2020-04-22 17:25:31 +0300

    create cards

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Chart , Cards , CountryPicker } from "./Components";
 import styles from "./App.module.css";
 
 const App = ()=>{
-  const [ mainData, setMainData ] = useState({})
+  const [ mainData, setMainData ] = useState(null)
   useEffect(()=>{
     const getData = async() =>{
       const data = await fetchData();
@@ -15,13 +15,16 @@ const App = ()=>{
     getData();
   }, []);
 
-  console.log(mainData)
+  if(!mainData){
+    return <p>loading...</p>
+  }
+
   return (
-    <div className={styles.container}>
-      <Cards />
-      <CountryPicker />
-      <Chart />
-    </div>
+    mainData && (<div className={styles.container}>
+      <Cards data={mainData}/>
+      <CountryPicker data={mainData}/>
+      <Chart data={mainData}/>
+    </div>)
   )
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,8 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async ()=>{
     try{
         const res = await axios.get(url);
-
-        return res.data;
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = res;
+        return { confirmed, recovered, deaths, lastUpdate } ;
     }catch(err){
 
     }
